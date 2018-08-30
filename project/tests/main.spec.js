2dd548b719403270a1c66ad328db7b5ba7646339
Lucas Carvalho <lucasccorrea@gmail.com>
2018-08-30 17:06:30 -0300

    add lib mocha and chai

---
+++ project/tests/main.spec.js
@@ -0,0 +1,17 @@
+describe('Main', function() {
+    describe('Method A', function() {
+      context('Case 1', function() {
+        it('should ...', function() {
+          throw new Error('an error here');
+        });
+      });
+  
+      context('Case 2', function() {
+
+        it('should ...', function() {
+          
+        });
+  
+      });
+    });
+});
