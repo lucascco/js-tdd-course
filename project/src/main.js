31a436004a481a45bd86da0e3e404a1c423809bc
Lucas Carvalho <lucasccorrea@gmail.com>
2018-09-03 15:25:25 -0300

    fix Eslint

--- project/src/main.js
+++ project/src/main.js
@@ -3,4 +3,4 @@ const sub = (n1, n2) => n1 - n2;
 const mult = (n1, n2) => n1 * n2;
 const divide = (n1, n2) => ((n2 === 0) ? 'Não pode ter divisão por zero' : (n1 / n2));
 
-export { sum, sub, mult, divide }
+export { sum, sub, mult, divide };
