97d1cf7351bb5c0c648d8107aff053f75e1f09ae
Lucas Carvalho <lucasccorrea@gmail.com>
2018-09-03 11:17:49 -0300

    include require of chai

--- project/tests/test.hooks.spec.js
+++ project/tests/test.hooks.spec.js
@@ -1,34 +1,35 @@
+let expect = require('chai').expect;
+
 describe.only('MochaHooks', function() {
     let arr;
-    before(function() {
-        console.log('before');
-    });
-
-    after(function() {
-        console.log('after');
-    });
 
     beforeEach(function() {
         arr = [1, 2, 3];
     });
 
-    afterEach(function() {
-        console.log('afterEach');
+    it('should be an array', function() {
+        expect(arr).to.be.a('array');
     });
 
-    it('show have a size of 4 when push another value to the array', function(){
+    it('should have a size of 4 when push another value to the array', function(){
         arr.push(4)
-        console.log(arr.length);
+        expect(arr).to.have.lengthOf(4);
     });
 
 
-    it('show have a size of 2 when pop from the the array', function(){
+    it('should have a size of 2 when pop from the the array', function(){
         arr.pop()
-        console.log(arr.length);
+        expect(arr).to.have.length(2)
     });
 
-    it('show have a size of 2 when pop from the the array', function(){
-        console.log(arr.pop() === 3);
+    it('should remove the value 3 when use pop in the array', function(){
+        arr.pop();
+        expect(arr).to.not.include(3)
+    });
+
+
+    it('should return true when pop 3 from the array', function(){
+        expect(arr.pop() === 3).to.be.true;
     });
 
 });
