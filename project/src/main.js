4902e784c6e98b496a0c4e2ff26506880324911e
Lucas Carvalho <lucasccorrea@gmail.com>
2018-09-03 15:44:34 -0300

    fix eslint

--- project/src/main.js
+++ project/src/main.js
@@ -5,10 +5,10 @@ const divide = (n1, n2) => ((n2 === 0) ? 'Não pode ter divisão por zero' : (n1
 
 
 const FizzBuzz = (num) => {
-    if(num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
-    if(num % 3 === 0) return 'Fizz';
-    if(num % 5 === 0) return 'Buzz';
-    return  num;
-}
+  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  if (num % 3 === 0) return 'Fizz';
+  if (num % 5 === 0) return 'Buzz';
+  return num;
+};
 
 export { sum, sub, mult, divide, FizzBuzz };
