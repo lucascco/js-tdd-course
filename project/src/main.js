21203ab8a71b2c2dbf7b3245f1abd58408453b9c
Lucas Carvalho <lucasccorrea@gmail.com>
2018-09-04 18:28:13 -0300

    fix lint

--- project/src/main.js
+++ project/src/main.js
@@ -1,8 +1,9 @@
 
-export const search = (query, type) =>
-  fetch(`https://api.spotify.com/v1/search/q=${ query }&type=${ type }`)
-  .then((data) => data.json());
+let fetch = require('node-fetch');
 
+export const search = (query, type) =>
+  fetch(`https://api.spotify.com/v1/search/q=${query}&type=${type}`)
+  .then((data) => data.json());
 
 export const searchAlbums = () => {
 
